35cae6bb7b8b6950fbecf9d2a77b3029f379957e
longpx <sdoai550575346>
2020-05-26 11:12:03 +0800

    Short -> Integer

---
+++ src/main/java/com/caspar/config/MyJavaTypeResolver.java
@@ -0,0 +1,35 @@
+package com.caspar.config;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl;
+
+/**
+ * Description:
+ *
+ * @author Caspar
+ * @since 2020/5/25 18:26
+ */
+public class MyJavaTypeResolver extends JavaTypeResolverDefaultImpl {
+
+    public MyJavaTypeResolver() {
+        super();
+    }
+
+    @Override
+    protected FullyQualifiedJavaType calculateBigDecimalReplacement(IntrospectedColumn column, FullyQualifiedJavaType defaultType) {
+        FullyQualifiedJavaType answer;
+
+        if (column.getScale() > 0 || column.getLength() > 18 || forceBigDecimals) {
+            answer = defaultType;
+        } else if (column.getLength() > 9) {
+            answer = new FullyQualifiedJavaType(Long.class.getName());
+        } else if (column.getLength() > 4) {
+            answer = new FullyQualifiedJavaType(Integer.class.getName());
+        } else {
+            answer = new FullyQualifiedJavaType(Integer.class.getName());
+        }
+
+        return answer;
+    }
+}
